4e636caa06bf64b79b1f10f93fd83a60cf100b59
Louis Dupas <louisdupas@net-c.com>
2019-12-19 08:47:04 +0100

    modif

--- app_game/src/App.js
+++ app_game/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Homepage from './components/Homepage/Homepage';
 import Choose from './components/name/Choose';
 import Choice from './components/chooseCards/Choice';
-import Choice from './components/chooseCards/Choice';
 import Choice from './components/trophy/Trophy';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
